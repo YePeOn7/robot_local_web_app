7c11ad2e479a08beec091593b1342f7f19f2f7c4
Yohan <prakoso.yohan@gmail.com>
2024-06-13 20:11:04 +0800

    add chart sample

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Sidebar from '@/components/Sidebar'
+import SampleCharts from '@/components/SampleCharts'
 
 const Content = () => {
   return (
     <div className='py-7  bg-primary flex-1'>
       <div className="content bg-bgColor h-full rounded-l-2xl px-4 py-2">
         <h1 className="text-2xl font-semibold"> Home Page</h1>
+        <SampleCharts/>
       </div>
     </div>
   )
