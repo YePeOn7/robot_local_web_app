3df4b9d580596a52b5dec808f96f5b690bc4a210
Yohan <prakoso.yohan@gmail.com>
2024-06-14 18:10:57 +0800

    update chart samples

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import Sidebar from '@/components/Sidebar'
 import { BatteryCard, IMUCard } from '@/components/InfoCard/InfoCard'
-import { SampleChart1, SampleChart2, SampleChart3 } from '@/components/SampleCharts'
+import { SampleAreaChart, SampleChart1, SampleChart2, SampleChart3 } from '@/components/SampleCharts'
 
 const Content = () => {
   return (
     <div className='py-7 h-screen bg-primary flex-1 '>
       <div className="content bg-bgColor h-full rounded-l-2xl px-6 py-4 overflow-y-scroll">
         <h1 className="text-2xl font-semibold mb-12 font-poppins"> Dashboard</h1>
-        <SampleChart1/>
-        <SampleChart2/>
-        <SampleChart3/>
+        <div className="grid grid-cols-1 md:grid-cols-2">
+          <SampleChart1 />
+          <SampleChart2 />
+          <SampleChart3 />
+          <SampleAreaChart />
+        </div>
         <div className='flex gap-3'>
-          <BatteryCard soc={82} voltage={51.2} current={1.2}/>
-          <IMUCard angle={0}/>
+          <BatteryCard soc={82} voltage={51.2} current={1.2} />
+          <IMUCard angle={0} />
         </div>
       </div>
     </div>
--- src/components/SampleCharts.tsx
+++ src/components/SampleCharts.tsx
@@ -7,7 +7,17 @@ import {
   Tooltip,
   ResponsiveContainer,
   Legend,
+  AreaChart,
+  Area,
+  TooltipProps,
 } from 'recharts';
+import { Payload } from 'recharts/types/component/DefaultLegendContent';
+
+interface CustomXAxisTickProps {
+  x: number;
+  y: number;
+  payload: Payload;
+}
 
 const SampleChart1 = () => {
 
@@ -26,20 +36,22 @@ const SampleChart1 = () => {
 
 
   return (
-    <LineChart width={400}
-      height={200} data={data}>
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="x" />
-      <YAxis yAxisId="left-axis" />
-      <YAxis yAxisId="right-axis"
-        orientation="right" />
-      <Line yAxisId="left-axis"
-        type="natural" dataKey="y"
-        stroke="orange" strokeWidth={3} />
-      <Line yAxisId="right-axis"
-        type="natural" dataKey="z"
-        stroke="blue" strokeWidth={3} />
-    </LineChart>
+    <ResponsiveContainer id='sample-chart-1' width="100%" height={200}>
+      <LineChart width={400}
+        height={200} data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="x" />
+        <YAxis yAxisId="left-axis" />
+        <YAxis yAxisId="right-axis"
+          orientation="right" />
+        <Line yAxisId="left-axis"
+          type="natural" dataKey="y"
+          stroke="orange" strokeWidth={3} />
+        <Line yAxisId="right-axis"
+          type="natural" dataKey="z"
+          stroke="blue" strokeWidth={3} />
+      </LineChart>
+    </ResponsiveContainer>
   );
 }
 
@@ -57,15 +69,17 @@ const SampleChart2 = () => {
   ];
 
   return (
-    <LineChart width={500} height={300} data={data}>
-      <XAxis dataKey="x" />
-      <YAxis />
-      <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-      <Line type="natural" dataKey="y" stroke="#8884d8" />
-      <Line type="natural" dataKey="z" stroke="#82ca9d" />
-      {/* <Line type="monotone" dataKey="amt" stroke="red" /> */}
-      <Tooltip />
-    </LineChart>
+    <ResponsiveContainer id='sample-chart-2' width="100%" height={200} >
+      <LineChart width={500} height={300} data={data}>
+        <XAxis dataKey="x" />
+        <YAxis />
+        <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
+        <Line type="natural" dataKey="y" stroke="#8884d8" />
+        <Line type="natural" dataKey="z" stroke="#82ca9d" />
+        {/* <Line type="monotone" dataKey="amt" stroke="red" /> */}
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
   );
 }
 
@@ -74,7 +88,7 @@ const SampleChart3 = () => {
   const generateData = () => {
     const data = [];
     const startTime = new Date('2024-06-14T10:00:00');
-    const voltageBase = 120;
+    const voltageBase = 30;
     const currentBase = 15;
 
     for (let i = 0; i < 20; i++) {
@@ -94,29 +108,99 @@ const SampleChart3 = () => {
 
   const data = generateData();
 
+  const formatTick = (tick:number) => tick.toFixed(1);
+
+  const tooltipStyles = {
+    container: {
+      backgroundColor: 'var(--primary-color)',
+      border: '1px solid #ccc',
+      padding: '5px',
+      fontSize: '12px', // Smaller font size
+      lineHeight: '1.2',
+      borderRadius: '10px'
+    },
+    label: {
+      margin: 0,
+      fontWeight: '500',
+    },
+    item: {
+      margin: 0,
+    },
+  };
+
+  const CustomTooltip: React.FC<TooltipProps<number, string>> = ({ active, payload, label }) => {
+    if (active && payload && payload.length) {
+      return (
+        <div className="custom-tooltip" style={tooltipStyles.container}>
+          <p style={tooltipStyles.label}>{`${label}`}</p>
+          {payload.map((entry, index) => (
+            <p key={`item-${index}`} style={tooltipStyles.item}>
+              {`${entry.name}: ${entry.value}`}
+            </p>
+          ))}
+        </div>
+      );
+    }
+  
+    return null;
+  };
+
   return (
-    <ResponsiveContainer width="100%" height={400}>
+    <ResponsiveContainer id='sample-chart-3' width="100%" height={200}>
       <LineChart
         data={data}
         margin={{
-          top: 10, right: 30, left: 0, bottom: 0,
+          top: 10, right: 40, left: 20, bottom: 0,
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="time" />
-        <YAxis yAxisId="left" label={{ value: 'Voltage (V)', angle: -90, position: 'insideLeft' }} domain={['dataMin - 100', 'dataMax + 100']} />
-        <YAxis yAxisId="right" orientation="right" label={{ value: 'Current (A)', angle: -90, position: 'insideRight' }} domain={['dataMin - 1', 'dataMax + 1']} />
-        <Tooltip />
+        <XAxis dataKey="time"/>
+        <YAxis yAxisId="left" tickFormatter={formatTick} label={{ value: 'Voltage (V)', angle: 90, position: 'insideTopLeft' }} domain={['dataMin - 10', 'dataMax + 10']} />
+        <YAxis yAxisId="right" tickFormatter={formatTick} orientation="right" label={{ value: 'Current (A)', angle: -90, position: 'insideTopRight' }} domain={['dataMin - 1', 'dataMax + 1']} />
+        <Tooltip content={<CustomTooltip />}/>
         <Legend />
-        <Line yAxisId="left" type="monotone" dataKey="voltage" stroke="#8884d8" dot={false}/>
-        <Line yAxisId="right" type="monotone" dataKey="current" stroke="#82ca9d" dot={false}/>
+        <Line yAxisId="left" type="monotone" dataKey="voltage" stroke="#8884d8" dot={false} strokeWidth={2} />
+        <Line yAxisId="right" type="monotone" dataKey="current" stroke="#82ca9d" dot={false} strokeWidth={2} />
       </LineChart>
     </ResponsiveContainer>
+  )
+}
+
+const SampleAreaChart = () => {
+   // Sample data
+   const data = [
+    { x: 1, y: 123, z: 122 },
+    { x: 2, y: 113, z: 713 },
+    { x: 3, y: 125, z: 312 },
+    { x: 4, y: 235, z: 123 },
+    { x: 5, y: 145, z: 420 },
+    { x: 6, y: 25, z: 529 },
+    { x: 7, y: 117, z: 61 },
+    { x: 8, y: 32, z: 435 },
+    { x: 9, y: 143, z: 93 },
+  ];
+
+  return (
+    <>
+    <ResponsiveContainer id='sample-area-chart' width='100%' height={200}>
+      <AreaChart width={500} height={200} data={data}>
+        <XAxis dataKey='x'/>
+        <YAxis/>
+
+
+        <Area dataKey='y' type='monotone' fill='yellow' stroke='yellow' stackId='1'/>
+        <Area dataKey='z' type='monotone' fill='green' stroke='green' stackId='1'/>
+        {/* <Area dataKey='y' type='monotone' fill='yellow' stroke='yellow'/>
+        <Area dataKey='z' type='monotone' fill='green' stroke='green'/> */}
+      </AreaChart>
+    </ResponsiveContainer>
+    </>
   )
 }
 
 export {
   SampleChart1,
   SampleChart2,
-  SampleChart3
+  SampleChart3,
+  SampleAreaChart
 };
